46b19b7b41f2fbfb251946e6c1de0dd071c69046
Donaldo Fernandez <donaldo.fndz@gmail.com>
2018-01-23 17:24:49 -0600

    Esta carpeta esta compuesta por las NEWS Las news se buscan por parametros especificos, falta merge con segunda seccion de tw

--- news/news.js
+++ news/news.js
@@ -1,8 +1,15 @@
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=mx&' +
-          'apiKey=006b195702b8403caba84617183f39b9';
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('006b195702b8403caba84617183f39b9');
+
+newsapi.v2.everything({
+  q: 'bitcoin',
+  sources: 'bbc-news,the-verge',
+  domains: 'bbc.co.uk, techcrunch.com',
+  from: '2017-12-01',
+  to: '2018-01-20',
+  language: 'en',
+  sortBy: 'relevancy',
+  page: 2
+}).then(response => {
+  console.log(response);
+})
